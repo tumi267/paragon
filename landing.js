cbccc79f8fc6565f9e37974c5d78c16e9c9b5539
tumi267 <itu.matlala4@gmail.com>
2026-01-13 05:18:43 +0200

    only commit

---
+++ landing.js
@@ -0,0 +1,19 @@
+const images = document.querySelectorAll('.image_left, .image_right, .image_center');
+
+const observerOptions = {
+  root: null,          // viewport
+  threshold: 0.35,      // trigger when 10% of image is visible
+};
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+          entry.target.classList.add('visible');   // fade in
+        } else {
+          entry.target.classList.remove('visible'); // fade out
+        }
+      });
+}, observerOptions);
+
+images.forEach(img => observer.observe(img));
+
